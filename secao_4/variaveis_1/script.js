72dc8bcc2d336adbc4493bdb6546268a371bb0eb
Luan Hii <luanhiivitor@gmail.com>
2022-07-25 02:13:16 -0300

    Seção 4, Aula 1

---
+++ secao_4/variaveis_1/script.js
@@ -0,0 +1,24 @@
+let nome = "Luan";
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "Vitor";
+
+console.log(nome);
+
+laranja = 900;
+
+console.log(laranja);
+
+laranja = "Laranja";
+
+console.log(laranja);
+
+let one = 1, two = 2, three = 3;
+
+console.log(one + two + three);
