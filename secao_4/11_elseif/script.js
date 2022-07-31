8ccf091f2ea3f18316be30752f34a461717b4098
Luan Hii <luanhiivitor@gmail.com>
2022-07-31 03:32:51 -0300

    Estruturas de repetição

---
+++ secao_4/11_elseif/script.js
@@ -0,0 +1,12 @@
+let nome = "Luan";
+let idade = 18;
+
+if (nome != undefined && nome == "Joaquim") {
+    console.log("Existe um nome");
+} else if (nome == "Luan" && nome.lenght > 5 && idade == 50);
+
+{
+    console.log("O nome é Luan");
+} else {
+    console.log("Não é Luan.")
+}
---
+++ secao_4/9_if/script.js
@@ -0,0 +1,10 @@
+let idade = 19;
+
+if (idade > 18) {
+    console.log('Entrada Permitida');
+}
+
+
+if (idade < 18) {
+    console.log('Entrada proibida');
+}
