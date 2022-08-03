83e58751c0fcb3220bebdb55c16f0253b403567b
Luan Hii <luanhiivitor@gmail.com>
2022-08-03 10:08:34 -0300

    Seção 4, Switch.

---
+++ secao_4/19_switch/script.js
@@ -0,0 +1,19 @@
+let nome = prompt("Digite seu nome"); //if e else são mais utilizados.
+
+switch (nome) {
+    case "Luan":
+        console.log("O seu nome é Luan");
+        break;
+
+    case "Vitor":
+        console.log("O seu nome é Vitor");
+        break;
+
+    case "Daniel":
+        console.log("O seu nome é Daniel");
+        break;
+
+    default:
+        console.log("Sem nome");
+        break;
+}
