19ca9901ef9c3fb3f557e059559e13f892f59e5c
LuanHii <luanhiivitor@gmail.com>
2023-08-09 23:52:08 -0300

    seção 6 - 02

---
+++ secao_6/02_mais_sobre/script.js
@@ -0,0 +1,23 @@
+function multiplicarTresNumeros(x,y,z){
+    return x * y * z;
+}
+
+console.log(multiplicarTresNumeros(2,3,4));
+
+const mult = multiplicarTresNumeros(3,4,5);
+
+console.log(mult);
+
+function podeDirigir(idade, cnh){
+    if (idade >=18 && cnh == true){
+        console.log("Pode dirigir");
+    } else {
+        console.log("Não pode dirigir");
+    }
+}
+
+console.log(podeDirigir(19,true));
+console.log(podeDirigir(25,true));
+console.log(podeDirigir(44,0));
+console.log(podeDirigir(19,1));
+console.log(podeDirigir(17,false));
