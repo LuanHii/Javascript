288e2986cc1fc149aa24c3bbf7bc1e2e262b68e2
LuanHii <luanhiivitor@gmail.com>
2023-08-09 22:59:09 -0300

    começando nova seção

---
+++ secao_6/01_definir_funcao/script.js
@@ -0,0 +1,19 @@
+function imprimirNoConsole(){
+    console.log("Imprimindo");
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num){
+    console.log("O numero é: " + num);
+}
+
+imprimirUmNumero(3);
+imprimirUmNumero(5);
+
+const numeroAleatorio = function() {
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+numeroAleatorio();
