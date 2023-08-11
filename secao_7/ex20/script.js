deacac69b4372b1e3dde84e704635a73008c7fbc
LuanHii <luanhiivitor@gmail.com>
2023-08-10 22:41:59 -0300

    ex20 adicionado

---
+++ secao_7/ex20/script.js
@@ -0,0 +1,10 @@
+function podeDirigir(x){
+   if (x >= 18){
+      console.log("Pode entrar na auto escola");
+   } else {
+      console.log("Não pode entrar na auto escola.");
+   }
+}
+
+podeDirigir(19);
+podeDirigir(14);
