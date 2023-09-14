fc9a046c4854a40112518fb38a46b2ac687e74fb
LuanHii <luanhiivitor@gmail.com>
2023-09-14 12:57:23 -0300

    final update

--- 19 JS6/01 _Let_Const/script.js
+++ 19 JS6/01 _Let_Const/script.js
@@ -170,3 +170,25 @@ console.log(shirt.productWithDiscount(10))
 
 const tenis = new Product("Tenis",30)
 console.log(tenis.productWithDiscount(10))
+
+// 9 herança
+
+class ProductWithAttributes extends Product {
+    constructor(name,price,colors){
+        super(name,price)
+        this.colors = colors
+    }
+
+    showColors() {
+        console.log("As cores são: ")
+        this.colors.forEach((color) => {
+            console.log(color)
+        })
+    }
+}
+
+const hat = new ProductWithAttributes("Chapeu", 30, ["Azul","Verde","Amarelo"]);
+
+console.log(hat.name)
+console.log(hat.productWithDiscount(30))
+console.log(hat.showColors())
