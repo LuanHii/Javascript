446292b1ec53649fb2a98ff91752c5e21a4ebe4b
LuanHii <luanhiivitor@gmail.com>
2023-09-13 22:51:28 -0300

    let e const

--- 19 JS6/01 _Let_Const/script.js
+++ 19 JS6/01 _Let_Const/script.js
@@ -1,1 +1,22 @@
-console.log("Hello World script.js");
+// 1 - var let e const
+var x = 10
+var y = 15
+
+if (y > 10) {
+    var x = 5
+    console.log (x)
+}
+
+console.log(x) // variavel x foi manchada pelo escopo local
+
+let a = 10
+let b = 15
+
+if (b > 10) {
+    let a = 5;
+    console.log(a)
+}
+
+console.log(a) // let nao mancha o escopo global
+
+
